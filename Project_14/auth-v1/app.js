edd6cd578f2778dd26fe32fb9f0a66cd17ea9eca
Bekzod Tolipov <btolipov110@gmail.com>
2022-07-27 16:08:33 -0500

    Add google authentication

--- Project_14/auth-v1/app.js
+++ Project_14/auth-v1/app.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passportLocalMongoose = require('passport-local-mongoose');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const TwitterStrategy = require('passport-twitter');
+const findOrCreate = require('mongoose-findorcreate');
 
 const bcrypt = require('bcrypt');
 
@@ -31,17 +34,51 @@ mongoose.connect('mongodb://localhost:27017/userDB');
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+     googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const UserModel = mongoose.model('User', userSchema);
 
 passport.use(UserModel.createStrategy());
 
-passport.serializeUser(UserModel.serializeUser());
-passport.deserializeUser(UserModel.deserializeUser());
+passport.use(new GoogleStrategy({
+    clientID: process.env.G_CLIENT_ID,
+    clientSecret: process.env.G_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    UserModel.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile)
+      return cb(err, user);
+    });
+  }
+));
+
+// passport.use(new TwitterStrategy({
+//     consumerKey: process.env.TWITTER_CLIENT_ID,
+//     consumerSecret: process.env.TWITTER_CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/twitter/secrets"
+//   },
+//   function(token, tokenSecret, profile, cb) {
+//     console.log(profile)
+//     UserModel.findOrCreate({ twitterId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    UserModel.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
 
 
 app.get('/', (req, res) => {
@@ -106,6 +143,27 @@ app.get('/logout', (req, res) => {
         res.redirect('/');
     });
 });
+
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile']}));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+
+
+
+
+// app.get('/auth/twitter', passport.authenticate('twitter'));
+ 
+// app.get('/auth/twitter/secrets',
+//   passport.authenticate('twitter', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//   });
 
 
 app.listen(3000, function() {
