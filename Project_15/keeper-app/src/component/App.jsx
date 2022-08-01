4a8dd9bc723287180ae27db8545a905d7722a99a
Bekzod Tolipov <btolipov110@gmail.com>
2022-08-01 12:10:57 -0500

    Creat first react app and app App Footer Header Note components

---
+++ Project_15/keeper-app/src/component/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Note from './Note';
+
+function App() {
+    return <div className="layout">
+        <Header />
+        <Note />
+        <Footer />
+    </div>;
+}
+
+export default App; 
